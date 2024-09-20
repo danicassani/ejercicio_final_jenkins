0f9c95b19e76a40384ec675bce5af054cc7bc57d
Daniel Durán Cassani <danielduran1995@gmail.com>
2024-09-20 12:52:34 +0200

    Add files via upload

---
+++ src/test/java/com/example/ejercicio_final/EjercicioFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
